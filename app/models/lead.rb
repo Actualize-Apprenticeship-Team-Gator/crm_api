8eb5381831152ed7666a16a863aa3f4b86e2d156
Paticia Morad <pjmorad@gmail.com>
2018-02-12 18:55:04 -0600

    166-erased new method that was accidentally created in lead.rb

--- app/models/lead.rb
+++ app/models/lead.rb
@@ -46,6 +46,4 @@ class Lead < ApplicationRecord
     # We tried dialing a valid number but the lead didn't answer their phone:
     self.contacted && !self.bad_number && !self.connected
   end
-
-
 end
