f5bccca3f9f72428a37b32dc5ced2eb9b6dd65c5
John Long <jplong91@gmail.com>
2018-02-19 20:06:33 -0600

    201 - add expand metho edkfjdskfd to app.js and v-on click expand to index.html.erb
    WIP 201 - Attempting to use VueJS to modifiy table
    
    201 - completed dropdown of lead events when lead is clicked on index page

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,25 +1,37 @@
 /* global Vue */
-document.addEventListener("DOMContentLoaded", function(event) { 
+document.addEventListener("DOMContentLoaded", function(event) {
   var app = new Vue({
-    el: '#app',
+    el: "#app",
     data: {
       leads: [],
+      // leadEvents: [],
       time_format: "12/25/17",
       url: "https://www.google.com/"
     },
     mounted: function() {
-      $.get('/api/v1/leads.json').success(function(response) {
-        console.log(this);
-        this.leads = response;
-      }.bind(this));
+      $.get("/api/v1/leads.json").success(
+        function(response) {
+          this.leads = response.map(function(lead) {
+            lead["showEvents"] = false;
+            return lead;
+          });
+        }.bind(this)
+      );
+      console.log(this.leads);
     },
     methods: {
       moment: function(date) {
         return moment(date);
+      },
+      expand: function(lead) {
+        $.get("/api/v1/leads/" + lead.id + ".json").success(
+          function(response) {
+            this.leadEvents = response.events;
+          }.bind(this)
+        );
+        lead.showEvents = !lead.showEvents;
       }
     },
-    computed: {
-
-    },
+    computed: {}
   });
 });
